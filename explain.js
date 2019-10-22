406451606a5d269c4b050db62424291afa8a07ff
吴文周 <wuwenzhou@qq.com.cn>
2019-10-22 11:52:35 +0800

    bug修复异常捕获

--- explain.js
+++ explain.js
@@ -4,14 +4,14 @@
  * @Github: https://github.com/fodelf
  * @Date: 2019-10-21 18:05:02
  * @LastEditors: 吴文周
- * @LastEditTime: 2019-10-22 01:56:05
+ * @LastEditTime: 2019-10-22 11:51:45
  */
 
 const http = require('https')
 /**
  * @name: get
  * @description:get请求
- * @param {String}:  scss样式查询关键字
+ * @param {String}:scss样式查询关键字
  * @return {}: 没有返回
  */
 function get(word) {
@@ -29,7 +29,8 @@ function get(word) {
         let body = ''
 
         if (rsp.statusCode >= 400) {
-          throw 'Translate failed, please check your network.'
+          // throw 'Translate failed, please check your network.'
+          resolve('')
         }
 
         rsp.on('data', function(data) {
@@ -38,35 +39,38 @@ function get(word) {
 
         rsp.on('end', function() {
           // 关键字匹配根据https://developer.mozilla.org文档格式解析
-          var article = body.match(/\<article((.|\n)*?)\<\/article\>/gi)[0]
-          var str = article.match(/\<dl((.|\n)*?)\<\/dl\>/gi)
-          var head = str[0].match(/\<dt((.|\n)*?)\<\/dt\>/gi).map(item => {
-            return item.replace(/<[^>]+>/g, '')
-          })
-          var headNext = str[1]
-            ? str[1].match(/\<dt((.|\n)*?)\<\/dt\>/gi).map(item => {
-                return item.replace(/<[^>]+>/g, '')
-              })
-            : []
-          var content = str[0].match(/\<dd((.|\n)*?)\<\/dd\>/gi).map(item => {
-            return item.replace(/<[^>]+>/g, '')
-          })
-          head.length = content.length
-          var contentNext = str[1]
-            ? str[1].match(/\<dd((.|\n)*?)\<\/dd\>/gi).map(item => {
-                return item.replace(/<[^>]+>/g, '')
-              })
-            : []
-          headNext.length = contentNext.length
-          content = content.concat(contentNext)
-          head = head.concat(headNext)
-          var back = ''
-          content.forEach((item, index) => {
-            back += head[index] + ':' + item + '\n\n'
-          })
-          resolve(back)
+          try {
+            var article = body.match(/\<article((.|\n)*?)\<\/article\>/gi)[0]
+            var str = article.match(/\<dl((.|\n)*?)\<\/dl\>/gi)
+            var head = str[0].match(/\<dt((.|\n)*?)\<\/dt\>/gi).map(item => {
+              return item.replace(/<[^>]+>/g, '')
+            })
+            var headNext = str[1]
+              ? str[1].match(/\<dt((.|\n)*?)\<\/dt\>/gi).map(item => {
+                  return item.replace(/<[^>]+>/g, '')
+                })
+              : []
+            var content = str[0].match(/\<dd((.|\n)*?)\<\/dd\>/gi).map(item => {
+              return item.replace(/<[^>]+>/g, '')
+            })
+            head.length = content.length
+            var contentNext = str[1]
+              ? str[1].match(/\<dd((.|\n)*?)\<\/dd\>/gi).map(item => {
+                  return item.replace(/<[^>]+>/g, '')
+                })
+              : []
+            headNext.length = contentNext.length
+            content = content.concat(contentNext)
+            head = head.concat(headNext)
+            var back = ''
+            content.forEach((item, index) => {
+              back += head[index].trim() + ' : ' + item + '\n\n'
+            })
+            resolve(back)
+          } catch (error) {
+            resolve('')
+          }
         })
-
         rsp.on('timeout', function() {
           reject(new Error('NetWork Connect Timeout.'))
         })
